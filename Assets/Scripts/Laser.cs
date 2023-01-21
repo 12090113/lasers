fd6417adc989ed19a93fccd611be319a29d8fbba
12090113 <47487266+12090113@users.noreply.github.com>
2023-01-20 22:00:51 -0800

    Fixed inconsistent laser width

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -44,7 +44,27 @@ public class Laser : MonoBehaviour
             points[i] = pos;
         }
 
-        lineRenderer.positionCount = bounces;
-        lineRenderer.SetPositions(points);
+        lineRenderer.positionCount = bounces * 10;
+        lineRenderer.SetPositions(Generate_Points(points));
+    }
+
+    Vector3[] Generate_Points(Vector3[] keyPoints, int segments = 10)
+    {
+        Vector3[] Points = new Vector3[(keyPoints.Length - 1) * segments + keyPoints.Length];
+        for (int i = 1; i < keyPoints.Length; i++)
+        {
+            Points[(i - 1) * segments + i - 1] = new Vector3(keyPoints[i - 1].x, keyPoints[i - 1].y, 0);
+            for (int j = 1; j <= segments; j++)
+            {
+                float x = keyPoints[i - 1].x;
+                float y = keyPoints[i - 1].y;
+                float z = 0;
+                float dx = (keyPoints[i].x - keyPoints[i - 1].x) / segments;
+                float dy = (keyPoints[i].y - keyPoints[i - 1].y) / segments;
+                Points[(i - 1) * segments + j + i - 1] = new Vector3(x + dx * j, y + dy * j, z);
+            }
+        }
+        Points[(keyPoints.Length - 1) * segments + keyPoints.Length - 1] = new Vector3(keyPoints[keyPoints.Length - 1].x, keyPoints[keyPoints.Length - 1].y, 0);
+        return Points;
     }
 }
